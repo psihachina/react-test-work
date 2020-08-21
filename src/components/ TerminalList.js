c79e22a13c64ed0252f429d8c42ff5fd38600c18
psihachina <psihachina@gmail.com>
2020-08-21 16:19:23 +0300

    Added animation for terminal list and alert

--- src/components/ TerminalList.js
+++ src/components/ TerminalList.js
@@ -1,17 +1,24 @@
 import React from 'react'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 export const TerminalList = ({ terminals, onRemove }) => {
     return (
-        <ul className="list-group mt-3">
+        <TransitionGroup component="ul" className="list-group mt-3">
             {terminals.map(terminal => (
-                <li class="list-group-item" key={terminal.id}>
-                <div className="row">
-                    <div className="col-4 d-flex align-items-center"><h5 className="mb-0">{terminal.title}</h5></div>
-                    <div className="col-6 d-flex align-items-center"><h6>{terminal.desc}</h6></div>
-                    <div className="col-2"><button className="btn btn-danger float-right" onClick={() => onRemove(terminal.id)}>Удалить</button></div>
-                </div>
-            </li>
+                <CSSTransition
+                    key={terminal.id}
+                    classNames={'terminal'}
+                    timeout={800}
+                >
+                    <li class="list-group-item">
+                        <div className="row">
+                            <div className="col-4 d-flex align-items-center"><h5 className="mb-0">{terminal.title}</h5></div>
+                            <div className="col-6 d-flex align-items-center"><h6>{terminal.desc}</h6></div>
+                            <div className="col-2"><button className="btn btn-danger float-right" onClick={() => onRemove(terminal.id)}>Удалить</button></div>
+                        </div>
+                    </li>
+                </CSSTransition>
             ))}
-        </ul>
+        </TransitionGroup>
     )
 }
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,19 +1,27 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { AlertContext } from '../context/alert/alertContext'
+import { CSSTransition } from 'react-transition-group'
 
 export const Alert = () => {
-    const {alert, hide} = useContext(AlertContext)
-
-    if (!alert.visible) {
-        return null
-    }
+    const { alert, hide } = useContext(AlertContext)
 
     return (
-        <div className={`alert alert-${alert.type || "warning"} alert-dismissible`}>
-            Внимание!&nbsp;{alert.text}
-            <button onClick={hide} type="button" className="close" aria-label="Close">
-                <span>&times;</span>
-            </button>
-        </div>
+        <CSSTransition
+            in={alert.visible}    
+            timeout={{
+                enter: 500,
+                exit: 350
+            }}
+            classNames={'alert'}
+            mountOnEnter
+            unmountOnExit
+        >
+            <div className={`alert alert-${alert.type || "warning"} alert-dismissible`}>
+                Внимание!&nbsp;{alert.text}
+                <button onClick={hide} type="button" className="close" aria-label="Close">
+                    <span>&times;</span>
+                </button>
+            </div>
+        </CSSTransition>
     )
 }
